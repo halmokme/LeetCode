9273999bbdd40e5f9325c79eecdefbfced043e36
halmokme <107684690+halmokme@users.noreply.github.com>
2023-01-13 14:12:28 +0900

    Time: 106 ms (85.18%), Space: 45.8 MB (97.53%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.js
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.js
@@ -4,10 +4,25 @@
  * @return {number}
  */
 const findMedianSortedArrays = function(nums1, nums2) {
-    let newArr = [...nums1, ...nums2].sort((a, b) => a - b);
-    if(newArr.length % 2) {
-        return newArr[newArr.length / 2 - 0.5];
-    } else {
-        return (newArr[newArr.length / 2 - 1] + newArr[newArr.length / 2]) / 2
+    if(nums1.length > nums2.length) return findMedianSortedArrays(nums2, nums1);
+    let x = nums1.length;
+    let y = nums2.length;
+    
+    let low = 0;
+    let high = x;
+    while(low <= high) {
+        let partitionX = parseInt((low + high) / 2);
+        let partitionY = parseInt((x + y + 1) / 2) - partitionX;
+        
+        let maxLeftX = partitionX === 0 ? - Infinity : nums1[partitionX - 1];
+        let minRightX = partitionX === x ? Infinity : nums1[partitionX];
+        
+        let maxLeftY = partitionY === 0 ? -Infinity : nums2[partitionY - 1];
+        let minRightY = partitionY === y ? Infinity : nums2[partitionY];
+        
+        if(maxLeftX <= minRightY && maxLeftY <= minRightX) {
+            return (x + y) % 2 !== 0 ? Math.max(maxLeftX, maxLeftY) : ((Math.max(maxLeftX, maxLeftY) + Math.min(minRightX, minRightY)) / 2);
+        } else if (maxLeftX > minRightY) high = partitionX-1;
+        else low = partitionX+1
     }
 };
