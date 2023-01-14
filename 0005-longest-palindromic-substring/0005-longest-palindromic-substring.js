a1166dbf78840686e7cdaba1f951134f7970868c
halmokme <107684690+halmokme@users.noreply.github.com>
2023-01-14 18:47:18 +0900

    Time: 7829 ms (5.00%), Space: 78.3 MB (7.76%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.js
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.js
@@ -2,26 +2,19 @@
  * @param {string} s
  * @return {string}
  */
+
+let dpVal = [];
 const longestPalindrome = function(s) {
     let maxLength = 0;
     let maxStr = '';
+    dpVal = Array.from(Array(s.length), () => new Array(s.length));
     
     if (s.length === 1) return s;
     
     for (let i=0; i<s.length; i++) {
         for (let j=i; j<s.length; j++) {
-            let palindrome = true;
             let len = j-i+1;
-            let left = i;
-            let right = j;
-            while(left < right) {
-                if(s.charAt(left) !== s.charAt(right)) {
-                    palindrome = false;
-                    break;
-                }
-                left++;
-                right--;
-            }
+            let palindrome = calc(s, i, j);
             if(palindrome && maxLength < len) {
                 maxLength = len;
                 maxStr = s.substring(i, j+1);
@@ -30,3 +23,13 @@ const longestPalindrome = function(s) {
     }
     return maxStr;
 };
+
+const calc = function(s, left, right) {
+    if(left > right) return true;
+    if(dpVal[left][right]) return true;
+    if(s.charAt(left) === s.charAt(right)) {
+        return calc(s, left+1, right-1);
+    } else {
+        return false;
+    }
+}
