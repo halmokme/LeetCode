bd24ea67e90b8ecdf5b2673f296ae48ebf47f393
halmokme <107684690+halmokme@users.noreply.github.com>
2023-01-15 00:09:28 +0900

    Time: 83 ms (91.93%), Space: 46.4 MB (77.04%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.js
+++ 0006-zigzag-conversion/0006-zigzag-conversion.js
@@ -4,35 +4,24 @@
  * @return {string}
  */
 const convert = function(s, numRows) {
-    let space1 = (numRows - 1) * 2;
-    let space2 = space1;
-    let space3 = 0;
-    let result = '';
-    
-    if(numRows === 1) return s;
-    
-    // 첫번째 행
-    for(let i=0; i<s.length; i+=space1) {
-        result += s[i];
+  if (numRows === 1) return s
+  
+  let result = ""
+  let space = numRows * 2 - 2
+  let diff1 = space, diff2 = 0
+
+  for (let i = 0; i < numRows; i++) {
+    let currStrIdx = i
+    let useFirst = true
+	while (currStrIdx < s.length) {
+	  useFirst = diff2 === 0 ? true : diff1 === 0 ? false : useFirst
+      result += s[currStrIdx]
+      useFirst ? currStrIdx += diff1 : currStrIdx += diff2
+      useFirst = !useFirst
     }
-    
-    // 중간 행
-    for(let j=1; j<numRows-1; j++) {
-        space2 -= 2;
-        space3 += 2;
-        toggle = true;
-        for(let i=j; i<s.length;) {
-            result += s[i];
-            if(toggle) i += space2;
-            else i += space3
-            toggle = !toggle;
-        }
-    }
-    
-    // 마지막 행
-    for(let i=numRows-1; i<s.length; i+=space1) {
-        result += s[i];
-    }
-    
-    return result;
-};
+    diff1 -= 2
+    diff2 += 2
+  }
+  
+  return result
+}
