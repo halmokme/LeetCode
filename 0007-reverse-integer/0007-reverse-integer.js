ed5eadaf7681329cd8501492d7752a51408a519c
halmokme <107684690+halmokme@users.noreply.github.com>
2023-01-16 02:47:10 +0900

    Time: 85 ms (62.41%), Space: 43.1 MB (93.64%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+const reverse = function(x) {
+    let result = String(x).split('').reverse().join('');
+    if (x < 0) result = - + result.slice(0, -1);
+    if(x === 0 || result >= 2**31-1 || result <= (-2)**31) return 0;
+    return result;
+};
