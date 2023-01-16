6f6a440510088ef317cb7a75dbc6f5173d800e4e
김재훈 <107684690+halmokme@users.noreply.github.com>
2023-01-17 01:21:22 +0900

    Time: 173 ms (84.75%), Space: 49.9 MB (96.87%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -6,7 +6,7 @@ const isPalindrome = function(x) {
     let num = x + '';
     let left = 0, right = num.length - 1;
     while(right > left) {
-        if(num[left] !== num[right]) return false;
+        if(num.charAt(left) !== num.charAt(right)) return false;
         left++;
         right--;
     }
