eb2510621deab8db9fe570bcb9e09340a00faa54
김재훈 <107684690+halmokme@users.noreply.github.com>
2023-01-19 06:14:09 +0900

    Time: 75 ms (88.12%), Space: 42.8 MB (99.56%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.js
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.js
@@ -4,8 +4,10 @@
  */
 const myAtoi = function(s) {
     let result = parseInt(s);
+    
     if(isNaN(result)) return 0;
     if(result > Math.pow(2, 31) - 1) return Math.pow(2, 31) - 1;
     else if(result < Math.pow(-2, 31)) return Math.pow(-2, 31);
+    
     return result;
 };
