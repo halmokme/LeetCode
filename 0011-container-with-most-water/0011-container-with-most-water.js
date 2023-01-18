25a3a0851c14d69c7ce68b4f71595672ea761ca7
김재훈 <107684690+halmokme@users.noreply.github.com>
2023-01-19 01:28:15 +0900

    Time: 84 ms (71.72%), Space: 49.5 MB (51.91%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+const maxArea = function(height) {
+    let size = 0, maxSize = 0;
+    let left = 0, right = height.length-1;
+    while(right > left) {
+        size = Math.min(height[left], height[right]) * (right - left);
+        maxSize = Math.max(size, maxSize);
+        if(height[left] > height[right]) {
+            right--;
+        } else {
+            left++;
+        }
+    }
+    return maxSize;
+};
