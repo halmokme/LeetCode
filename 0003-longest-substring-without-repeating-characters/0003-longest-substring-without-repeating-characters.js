b2c5d20cb873ef9239d2aada1f1323b2641d99f0
김재훈 <107684690+halmokme@users.noreply.github.com>
2023-01-19 06:10:31 +0900

    Time: 91 ms (84.07%), Space: 47.8 MB (55.08%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -3,8 +3,7 @@
  * @return {number}
  */
 const lengthOfLongestSubstring = function(s) {
-    let storage = '';
-    let maxLength = 0;
+    let storage = '', maxLength = 0;
     for(let i=0; i<s.length; i++) {
         if(storage.includes(s[i])) {
             storage = storage.slice(storage.indexOf(s[i])+1);
