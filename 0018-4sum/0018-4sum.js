dd133151b57f4e8a4befd94858e68c8e5bda586b
김재훈 <107684690+halmokme@users.noreply.github.com>
2023-01-26 00:41:08 +0900

    Time: 105 ms (62.33%), Space: 44.2 MB (64.83%) - LeetHub

---
+++ 0018-4sum/0018-4sum.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[][]}
+ */
+const fourSum = function(nums, target) {
+    let n = nums.sort((a, b) => a - b);
+    let result = [];
+    for(let i=0; i<n.length-3; i++) {
+        if( i > 0 && n[i] === n[i-1]) continue;
+        for(let j=i+1; j<n.length-2; j++) {
+            let left = j+1;
+            let right = n.length-1;
+            if( j > i+1 && n[j] === n[j-1]) continue;
+            while(right > left) {
+                let sum = n[i] + n[j] + n[left] + n[right];
+                if(sum === target) {
+                    result.push([n[i], n[j], n[left], n[right]]);
+                    left++;
+                    right--;
+                    while(n[left] === n[left-1]) left++;
+                    while(n[right] === n[right+1]) right--;
+                }
+                else if(sum < target) left++;
+                else right--;
+            }
+        }
+    }
+    return result;
+};
