c7fd4208b8d42c3ede7e14b35be7c77a79d10c14
김재훈 <107684690+halmokme@users.noreply.github.com>
2023-01-29 23:24:22 +0900

    Time: 73 ms (74.47%), Space: 44.6 MB (17.37%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
@@ -11,8 +11,7 @@
  * @return {ListNode}
  */
 const mergeTwoLists = function(list1, list2) {
-    if(!list1) return list2;
-    if(!list2) return list1;
+    if(!list1 || !list2) return (list1 ? list1 : list2)
     if(list1.val <= list2.val) {
         list1.next = mergeTwoLists(list1.next, list2);
         return list1;
