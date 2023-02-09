42925a0504238c41303645924b1571bc342af03e
김재훈 <107684690+halmokme@users.noreply.github.com>
2023-02-10 01:06:52 +0900

    Time: 68 ms (58.15%), Space: 42.4 MB (21.31%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+const search = function(nums, target) {
+    
+    let left = 0, right = nums.length-1;
+
+    while(right >= left) {
+        let mid = Math.floor((left + right) / 2);
+        if(nums[mid] === target) return mid;
+
+        if(nums[mid] >= nums[left]) {
+            if(nums[mid] > target && target >= nums[left]) right = mid - 1;
+            else left = mid + 1;
+        } else {
+            if(nums[mid] < target && target <= nums[right]) left = mid + 1;
+            else right = mid - 1;
+        }
+    }
+    return -1;
+};
