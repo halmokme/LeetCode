6bb91c887c5776f239401fa571682dd53349cfae
김재훈 <107684690+halmokme@users.noreply.github.com>
2023-02-18 00:37:14 +0900

    Time: 82 ms (66.90%), Space: 45.9 MB (93.52%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.js
+++ 0041-first-missing-positive/0041-first-missing-positive.js
@@ -3,13 +3,19 @@
  * @return {number}
  */
 const firstMissingPositive = function(nums) {
-    let arr = [... new Set(nums)];
-    arr = arr.sort((a, b) => a - b).filter(el => el > 0);
+    for (let i = 0; i < nums.length; i++) {
+        let idx = nums[i]-1;
+        if (i === idx || nums[i] === nums[idx]) continue;
+        if (idx >= 0 && idx <= nums.length - 1) {
+            [nums[i], nums[idx]] = [nums[idx], nums[i]];
+            i--;
+        }
+    }
     
-    for (let i = 0; i < arr.length; i++) {
-        if (i+1 == arr[i]) continue;
+    for (let i = 0; i < nums.length; i++) {
+        if (i+1 === nums[i]) continue;
         else return i+1;
     }
     
-    return arr.length + 1;
+    return nums.length + 1;
 };
