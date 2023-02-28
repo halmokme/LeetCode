bf61d358c2ce80f0f4293cbc95a5586e7986a32b
김재훈 <107684690+halmokme@users.noreply.github.com>
2023-02-28 10:35:39 +0900

    Time: 52 ms (95.74%), Space: 42.4 MB (8.98%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.js
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -3,7 +3,6 @@
  * @return {number}
  */
 const lengthOfLastWord = function(s) {
-    // let str = s.trim();
-    // return str.length - (str.lastIndexOf(' ') + 1);
-    return s.split(' ').filter(el => el+'').at(-1).length;
+    return s.trim().length - (s.trim().lastIndexOf(' ') + 1);
+    // return s.split(' ').filter(el => el+'').at(-1).length;
 };
