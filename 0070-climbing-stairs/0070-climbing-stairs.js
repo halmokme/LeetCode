4257ed33df9cd03f369ee350fe56d564205ca86d
김재훈 <107684690+halmokme@users.noreply.github.com>
2023-03-04 08:54:31 +0900

    Time: 64 ms (54.44%), Space: 41.8 MB (46.49%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.js
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -3,10 +3,10 @@
  * @return {number}
  */
 const climbStairs = function(n) {
-    let memo = [0,1,2,3];
-    if(n <= 3) return memo[n];
+    let memo = [0,1,2];
+    if(n <= 2) return memo[n];
     
-    for(let i=4; i<=n; i++) {
+    for(let i=3; i<=n; i++) {
         memo[i] = memo[i-1] + memo[i-2];
     }
     return memo[n];
