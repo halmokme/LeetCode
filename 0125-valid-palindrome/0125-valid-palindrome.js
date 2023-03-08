3db0da29292429c305d0e5268757831e5aa0da50
김재훈 <107684690+halmokme@users.noreply.github.com>
2023-03-09 00:04:48 +0900

    Time: 76 ms (67.28%), Space: 47 MB (33.21%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -4,14 +4,17 @@
  */
 const isPalindrome = function(s) {
     let str = s.toLowerCase().replace(/[^a-z0-9]/g, '');
-    let left = 0, right = str.length-1;
-    while(right >= left) {
-        if(str[left] === str[right]) {
-            left++;
-            right--;
-        } else {
-            return false
-        }
-    }
-    return true;
+    let reverse = str.split('').reverse().join('');
+    return str === reverse;
+    
+    // let left = 0, right = str.length-1;
+    // while(right >= left) {
+    //     if(str[left] === str[right]) {
+    //         left++;
+    //         right--;
+    //     } else {
+    //         return false
+    //     }
+    // }
+    // return true;
 };
