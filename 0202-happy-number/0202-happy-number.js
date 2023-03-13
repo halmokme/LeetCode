8699afdee4c35584361f0895eb99e83c16262d7a
김재훈 <107684690+halmokme@users.noreply.github.com>
2023-03-14 00:19:17 +0900

    Time: 68 ms (70.45%), Space: 44.2 MB (30.19%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+const isHappy = function(n) {
+    
+    // recursive
+    let res = true;
+    const recursive = (sum, arr) => {
+        sum = (sum+'').split('').reduce((acc, val) => acc + Math.pow(val, 2), 0);
+        if(sum === 1) return;
+        if(arr.indexOf(sum) !== -1) {
+            res = false;
+            return;
+        };
+        arr.push(sum);
+        recursive(sum, arr);
+    }
+    recursive(n, []);
+    return res;
+    
+    
+    
+    // while loop
+    let sum = n;
+    let stack = [];
+    while(1) {
+        sum = (sum+'').split('').reduce((acc, val) => acc + Math.pow(val, 2), 0);
+        if(stack.indexOf(sum) > -1) return false;
+        if(sum === 1) return true;
+        stack.push(sum);
+    }
+
+};
