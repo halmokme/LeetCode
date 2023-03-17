935388cc5ea48940e920d727f19e73dc662eec39
김재훈 <107684690+halmokme@users.noreply.github.com>
2023-03-18 00:15:18 +0900

    Time: 59 ms (98.22%), Space: 43.4 MB (68.77%) - LeetHub

--- 0258-add-digits/0258-add-digits.js
+++ 0258-add-digits/0258-add-digits.js
@@ -3,8 +3,15 @@
  * @return {number}
  */
 const addDigits = function(num) {
-    while(1) {
-        num = (num+'').split('').reduce((acc, val) => acc + +val, 0);
-        if(num < 10) return num;
-    }
+    // while(1) {
+    //     num = (num+'').split('').reduce((acc, val) => acc + +val, 0);
+    //     if(num < 10) return num;
+    // }
+  if (num === 0) {
+    return 0;
+  } else if (num % 9 === 0) {
+    return 9;
+  } else {
+    return num % 9;
+  }
 };
