c1c12a96dff013114dc5c5a42eca7255d86d8cb1
김재훈 <107684690+halmokme@users.noreply.github.com>
2023-03-22 20:47:50 +0900

    Time: 51 ms (94.80%), Space: 42.5 MB (11.70%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} pattern
+ * @param {string} s
+ * @return {boolean}
+ */
+const wordPattern = function(pattern, s) {
+
+    let arr = s.split(' '), tmp = {};
+
+    if(pattern.length !== arr.length || new Set([...pattern]).size !== new Set(arr).size) return false;
+
+    for(let i=0; i<pattern.length; i++) {
+        if(!tmp[pattern[i]]) tmp[pattern[i]] = arr[i];
+        else if(tmp[pattern[i]] !== arr[i]) return false;
+    }
+    return true;
+};
