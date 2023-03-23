f4a3db0c484be2976cfddc2c41265cec36abd77b
김재훈 <107684690+halmokme@users.noreply.github.com>
2023-03-24 00:10:53 +0900

    Time: 205 ms (89.75%), Space: 52.4 MB (29.92%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.ts
+++ 0326-power-of-three/0326-power-of-three.ts
@@ -1,12 +1,12 @@
 function isPowerOfThree(n: number): boolean {
 
     // while loop
-    // if(n === 0) return false;
-    // while(n % 3 === 0) {
-    //     n /= 3;
-    // }
-    // return n === 1;
+    if(n === 0) return false;
+    while(n % 3 === 0) {
+        n /= 3;
+    }
+    return n === 1;
     
-    const expo = (Math.log(n) / Math.log(3)).toFixed(10);
-    return +expo % 1 === 0 ? true : false;
+    // const expo = (Math.log(n) / Math.log(3)).toFixed(10);
+    // return +expo % 1 === 0 ? true : false;
 };
