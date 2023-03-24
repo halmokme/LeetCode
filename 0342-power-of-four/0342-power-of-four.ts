5b4d585f149db9fee3bbd5027d79fed373289a35
김재훈 <107684690+halmokme@users.noreply.github.com>
2023-03-25 02:55:15 +0900

    Time: 83 ms (41.46%), Space: 44.4 MB (91.46%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.ts
+++ 0342-power-of-four/0342-power-of-four.ts
@@ -1,7 +1,9 @@
 function isPowerOfFour(n: number): boolean {
-    if(n === 0) return false;
-    while(n % 4 === 0) {
-        n /= 4;
-    }
-    return n === 1;
+    // if(n === 0) return false;
+    // while(n % 4 === 0) {
+    //     n /= 4;
+    // }
+    // return n === 1;
+    const expo = (Math.log(n) / Math.log(4)).toFixed(10);
+    return +expo % 1 === 0 ? true : false;
 };
