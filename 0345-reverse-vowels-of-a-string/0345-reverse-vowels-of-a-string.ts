9ab80ac713b7588239b849158bb6744271417caf
김재훈 <107684690+halmokme@users.noreply.github.com>
2023-03-27 23:04:36 +0900

    Time: 80 ms (80.37%), Space: 48.3 MB (93.46%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.ts
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.ts
@@ -5,13 +5,13 @@ function reverseVowels(s: string): string {
     let vowel = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
     
     while(right > left) {
-        if(vowel.indexOf(arr[left]) === -1) left++;
-        if(vowel.indexOf(arr[right]) === -1) right--;
         if(vowel.indexOf(arr[left]) > -1 && vowel.indexOf(arr[right]) > -1 ) {
             [arr[left], arr[right]] = [arr[right], arr[left]];
             left++;
             right--;
         }
+        if(vowel.indexOf(arr[left]) === -1) left++;
+        if(vowel.indexOf(arr[right]) === -1) right--;
     }
     return arr.join('');
 };
