335861f36af33b57b0210c1a0d18aaee1da705b9
김재훈 <107684690+halmokme@users.noreply.github.com>
2023-03-28 00:01:27 +0900

    Time: 62 ms (83.87%), Space: 44.7 MB (70.51%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.ts
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.ts
@@ -1,13 +1,6 @@
 function intersection(nums1: number[], nums2: number[]): number[] {
-    // const num1 = new Set(nums1);
-    // const num2 = new Set(nums2);
-    // const intersect = new Set([...num1].filter(el => num2.has(el)));
-    // return [...intersect];
-    let set = new Set(nums1);
-    for(let item of set) {
-        if(!nums2.includes(item)) {
-            set.delete(item);
-        }
-    }
-    return [...set];
+    const num1 = new Set(nums1);
+    const num2 = new Set(nums2);
+    const intersect = new Set([...num1].filter(el => num2.has(el)));
+    return [...intersect];
 }
