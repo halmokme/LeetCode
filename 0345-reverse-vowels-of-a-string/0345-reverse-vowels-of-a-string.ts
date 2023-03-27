da8b1507f3109f5abb897a30e7c6f3faedd74aa5
김재훈 <107684690+halmokme@users.noreply.github.com>
2023-03-27 23:02:30 +0900

    Time: 87 ms (59.81%), Space: 48.7 MB (76.64%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.ts
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.ts
@@ -3,22 +3,11 @@ function reverseVowels(s: string): string {
     let arr = s.split('');
     let left = 0, right = s.length - 1;
     let vowel = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
-    let findLeft = false, findRight = false;
     
     while(right > left) {
-        if(vowel.indexOf(arr[left]) > -1) {
-            findLeft = true;
-        } else {
-            left++;
-            findLeft = false;
-        }
-        if(vowel.indexOf(arr[right]) > -1) {
-            findRight = true;
-        } else {
-            right--;
-            findRight = false;
-        }
-        if(findLeft && findRight) {
+        if(vowel.indexOf(arr[left]) === -1) left++;
+        if(vowel.indexOf(arr[right]) === -1) right--;
+        if(vowel.indexOf(arr[left]) > -1 && vowel.indexOf(arr[right]) > -1 ) {
             let tmp = s[left];
             arr[left] = s[right];
             arr[right] = tmp;
