61f8db03a176810a95ebc7f8d6bc13ab41f1f942
김재훈 <107684690+halmokme@users.noreply.github.com>
2023-04-01 00:35:02 +0900

    Time: 92 ms (59.68%), Space: 47 MB (41.60%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.ts
+++ 0383-ransom-note/0383-ransom-note.ts
@@ -1,9 +1,12 @@
 function canConstruct(ransomNote: string, magazine: string): boolean {
-    let arr = magazine.split('')
+    
+    let arr = magazine.split('');
+    
     for(let i=0; i<ransomNote.length; i++) {
         let idx = arr.indexOf(ransomNote[i]);
         if(idx === -1) return false;
         else arr.splice(idx, 1);
     }
+    
     return true;
 };
