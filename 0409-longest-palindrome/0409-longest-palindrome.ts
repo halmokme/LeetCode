81e1c36066aee0ac1b65c6984d17a32f4f69ce28
김재훈 <107684690+halmokme@users.noreply.github.com>
2023-04-06 22:18:08 +0900

    Time: 72 ms (59.03%), Space: 45.2 MB (46.32%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.ts
+++ 0409-longest-palindrome/0409-longest-palindrome.ts
@@ -1,16 +1,17 @@
 function longestPalindrome(s: string): number {
     
     let dic = {};
-    for(let i=0; i<s.length; i++) {
-        if(dic[s[i]]) dic[s[i]]++;
-        else dic[s[i]] = 1;
+    for(let char of s) {
+        if(dic[char]) dic[char]++;
+        else dic[char] = 1;
     }
 
-    let res = 0, odd = false;
+    let res = 0
+    let isOdd = false;
     for(let char in dic) {
-        if(dic[char] % 2 > 0) odd = true;
+        if(dic[char] % 2 > 0) isOdd = true;
         res += Math.floor(dic[char] / 2) * 2
     }
-    return odd ? res+1 : res;
+    return isOdd ? res+1 : res;
 };
 
