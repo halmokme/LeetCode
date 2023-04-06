1bbd876605d3262b59c0554bf9f639d3af567365
김재훈 <107684690+halmokme@users.noreply.github.com>
2023-04-07 00:36:15 +0900

    Time: 68 ms (76.33%), Space: 45.1 MB (68.09%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.ts
@@ -0,0 +1,12 @@
+function fizzBuzz(n: number): string[] {
+    if(n === 1) return ['1'];
+    let res = ['1', '2', 'Fizz'];
+    for(let i=3; i<=n; i++) {
+        if(i % 3 === 0 && i % 5 === 0) res[i-1] = 'FizzBuzz';
+        else if(i % 3 === 0) res[i-1] = 'Fizz';
+        else if(i % 5 === 0) res[i-1] = 'Buzz';
+        else res[i-1] = i+'';
+    }
+    return res;
+};
+
