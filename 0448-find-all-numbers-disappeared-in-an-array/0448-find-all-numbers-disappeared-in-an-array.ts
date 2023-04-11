36bdac8021b9ca6731fed7f99be0e65a1a459316
김재훈 <107684690+halmokme@users.noreply.github.com>
2023-04-11 22:59:10 +0900

    Time: 103 ms (79.19%), Space: 50.4 MB (94.51%) - LeetHub

---
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.ts
@@ -0,0 +1,12 @@
+function findDisappearedNumbers(nums: number[]): number[] {
+  let res = [];
+  for (let i=0; i<nums.length; i++) {
+      let num = Math.abs(nums[i]);
+      nums[num-1] = Math.abs(nums[num-1]) * -1;
+  }
+  console.log(nums)
+  for (let i=0; i<nums.length; i++) {
+      if (nums[i] > 0) res.push(i+1);
+  }
+  return res;
+};
