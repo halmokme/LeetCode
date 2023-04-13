8c0da01434ad8ca542adb7fe8c7c1cf7a1a26204
김재훈 <107684690+halmokme@users.noreply.github.com>
2023-04-13 23:25:45 +0900

    Time: 75 ms (67.02%), Space: 45.2 MB (82.98%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.ts
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.ts
@@ -1,12 +1,10 @@
 function repeatedSubstringPattern(s: string): boolean {
+    
+    /*
+    1. s를 붙여 새로운 문자열을 만든다.
+    2. 양쪽 끝의 문자 하나씩을 제외한 문자열에서 s가 나왔다면 s는 substring pattern이다.
+    */
 
-    if(s.length < 2) return false;
-    
-    let pattern = '';
-    for(let i=0; i<s.length/2; i++) {
-        pattern += s[i];
-        if(!s.split(pattern).join('').length) return true;
-    }
-    
-    return false;
+    let str = (s + s).substring(1, s.length * 2 - 1);
+    return str.indexOf(s) !== -1;
 };
