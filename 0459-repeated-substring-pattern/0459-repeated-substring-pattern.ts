c385ec2713e536aeb988502b0d65ff3abdd54ed9
김재훈 <107684690+halmokme@users.noreply.github.com>
2023-04-13 23:16:43 +0900

    Time: 1273 ms (6.38%), Space: 50.1 MB (15.96%) - LeetHub

---
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.ts
@@ -0,0 +1,12 @@
+function repeatedSubstringPattern(s: string): boolean {
+
+    if(s.length < 2) return false;
+    
+    let pattern = '';
+    for(let i=0; i<s.length/2; i++) {
+        pattern += s[i];
+        if(s.split(pattern).join('').length === 0) return true;
+    }
+    
+    return false;
+};
