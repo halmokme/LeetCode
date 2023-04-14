49b19a7ed1cc0acc1f249cfe53f937140ffa4ab9
김재훈 <107684690+halmokme@users.noreply.github.com>
2023-04-14 21:54:26 +0900

    Time: 61 ms (80.56%), Space: 43.9 MB (25.00%) - LeetHub

--- 0476-number-complement/0476-number-complement.ts
+++ 0476-number-complement/0476-number-complement.ts
@@ -1,5 +1,11 @@
 function findComplement(num: number): number {
     let binarr = num.toString(2).split('');
-    let reverse = binarr.map(el => el === '1' ? 0 : el === '0' ? 1 : el).join('');
+    
+    let reverse = binarr.map(el => {
+        if(el === '1') return 0;
+        else if(el === '0') return 1;
+        else return el;
+    }).join('');
+    
     return parseInt(reverse, 2);
 };
