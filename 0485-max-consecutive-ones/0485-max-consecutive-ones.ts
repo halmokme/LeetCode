942207a2174f1f8147e399c9245de701d9674ec3
김재훈 <107684690+halmokme@users.noreply.github.com>
2023-04-15 22:21:45 +0900

    Time: 75 ms (49.35%), Space: 45.5 MB (51.96%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.ts
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.ts
@@ -2,9 +2,9 @@ function findMaxConsecutiveOnes(nums: number[]): number {
 
     let max = 0;
     let count = 0;  
-    while(nums.length) {
-        let num = nums.pop();
-        if(num === 1) count++;
+
+    for(let i=0; i<nums.length; i++) {
+        if(nums[i] === 1) count++;
         else count = 0;
         max = Math.max(max, count);
     }
