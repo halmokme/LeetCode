830b4608ab7871e1cdbdb62a6cddc16c298c8fb0
김재훈 <107684690+halmokme@users.noreply.github.com>
2023-04-18 23:24:43 +0900

    Time: 67 ms (31.43%), Space: 42.9 MB (51.43%) - LeetHub

---
+++ 0500-keyboard-row/0500-keyboard-row.ts
@@ -0,0 +1,24 @@
+function findWords(words: string[]): string[] {
+    const res = [];
+    const keyboard = { 1: 'qwertyuiop', 2: 'asdfghjkl', 3: 'zxcvbnm' };
+    let i = 0
+    let rowIdx = 1;
+    
+    while(i < words.length) {
+        if(words[i].toLowerCase().split('').every((char) => keyboard[rowIdx].includes(char))) {
+          res.push(words[i]);
+            i++;
+            rowIdx = 1;
+        } else {
+            if(rowIdx === 3) {
+                i++;
+                rowIdx = 1;
+            }
+            else {
+                rowIdx++;    
+            }
+        }
+    }
+    
+    return res;
+};
