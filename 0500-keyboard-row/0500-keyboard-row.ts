5984860fc6313d58f78707093b020b989c08ce55
김재훈 <107684690+halmokme@users.noreply.github.com>
2023-04-18 23:25:32 +0900

    Time: 66 ms (42.86%), Space: 42.6 MB (77.14%) - LeetHub

--- 0500-keyboard-row/0500-keyboard-row.ts
+++ 0500-keyboard-row/0500-keyboard-row.ts
@@ -1,8 +1,8 @@
 function findWords(words: string[]): string[] {
-    const res = [];
-    const keyboard = { 1: 'qwertyuiop', 2: 'asdfghjkl', 3: 'zxcvbnm' };
-    let i = 0
-    let rowIdx = 1;
+    const res = [], 
+          keyboard = { 1: 'qwertyuiop', 2: 'asdfghjkl', 3: 'zxcvbnm' };
+    let i = 0,
+        rowIdx = 1;
     
     while(i < words.length) {
         if(words[i].toLowerCase().split('').every((char) => keyboard[rowIdx].includes(char))) {
