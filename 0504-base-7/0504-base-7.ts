0b5c87a70cdb7eebb497913732de94a83c521429
김재훈 <107684690+halmokme@users.noreply.github.com>
2023-04-19 23:36:48 +0900

    Time: 66 ms (45.00%), Space: 43 MB (80.00%) - LeetHub

--- 0504-base-7/0504-base-7.ts
+++ 0504-base-7/0504-base-7.ts
@@ -1,14 +1,15 @@
 function convertToBase7(num: number): string {
-    if (num === 0) return '0';
+//     if (num === 0) return '0';
     
-    let res = '';
-    let sign = num >= 0;
-    num = Math.abs(num);
+//     let res = '';
+//     let sign = num >= 0;
+//     num = Math.abs(num);
     
-    while (num > 0) {
-        res = num % 7 + res;
-        num = num / 7 | 0;
-    }
+//     while (num > 0) {
+//         res = num % 7 + res;
+//         num = num / 7 | 0;
+//     }
     
-    return sign ? res : '-' + res;
+//     return sign ? res : '-' + res;
+    return num.toString(7)
 };
