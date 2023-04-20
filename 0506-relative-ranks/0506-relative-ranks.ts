9548962066a95f9b777f243de8f7d314b06a9d80
김재훈 <107684690+halmokme@users.noreply.github.com>
2023-04-20 22:44:05 +0900

    Time: 127 ms (34.69%), Space: 45.7 MB (65.31%) - LeetHub

--- 0506-relative-ranks/0506-relative-ranks.ts
+++ 0506-relative-ranks/0506-relative-ranks.ts
@@ -2,11 +2,11 @@ function findRelativeRanks(score: number[]): string[] {
     const arr = [...score].sort((a, b) => b - a);
     let res = [];
     score.map(el => {
-        let idx = arr.indexOf(el);
-        if(idx === 0) res.push('Gold Medal');
-        else if(idx === 1) res.push('Silver Medal');
-        else if(idx === 2) res.push('Bronze Medal');
-        else res.push(idx+1+'');
+        let idx = (arr.indexOf(el) + 1).toString();
+        if(idx === '1') res.push('Gold Medal');
+        else if(idx === '2') res.push('Silver Medal');
+        else if(idx === '3') res.push('Bronze Medal');
+        else res.push(idx);
     })
     return res;
 };
