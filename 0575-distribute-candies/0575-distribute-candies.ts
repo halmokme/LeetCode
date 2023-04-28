217f132921f29cc36555f7dcfe871a88c0eefbda
김재훈 <107684690+halmokme@users.noreply.github.com>
2023-04-29 00:15:00 +0900

    Time: 107 ms (78.05%), Space: 68 MB (7.32%) - LeetHub

--- 0575-distribute-candies/0575-distribute-candies.ts
+++ 0575-distribute-candies/0575-distribute-candies.ts
@@ -1,5 +1,5 @@
 function distributeCandies(candyType: number[]): number {
     let typeCount = [... new Set(candyType)].length;
     let candyHalf = Math.floor(candyType.length / 2);
-    return typeCount > candyHalf ? candyHalf : typeCount;
+    return Math.min(typeCount, candyHalf);
 };
