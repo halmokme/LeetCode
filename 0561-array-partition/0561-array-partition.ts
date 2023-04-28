d179cdc69e124d5113076c6fcb8525239b8c99bb
김재훈 <107684690+halmokme@users.noreply.github.com>
2023-04-28 23:18:41 +0900

    Time: 126 ms (46.75%), Space: 49.4 MB (46.75%) - LeetHub

--- 0561-array-partition/0561-array-partition.ts
+++ 0561-array-partition/0561-array-partition.ts
@@ -1,4 +1,4 @@
 function arrayPairSum(nums: number[]): number {
     return nums.sort((a, b) => a - b)
-    .reduce((acc, val, idx) => acc + (idx % 2 === 0 && val));
+    .reduce((acc, val, idx) => acc + (idx % 2 === 0 && val), 0);
 };
