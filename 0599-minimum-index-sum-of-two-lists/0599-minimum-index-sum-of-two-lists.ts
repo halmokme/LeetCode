273c2fff3929a356bb18c40b436fbb7452c5a8c9
김재훈 <107684690+halmokme@users.noreply.github.com>
2023-05-01 23:02:24 +0900

    Time: 72 ms (100.00%), Space: 50 MB (53.49%) - LeetHub

--- 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.ts
+++ 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.ts
@@ -1,16 +1,23 @@
 function findRestaurant(list1: string[], list2: string[]): string[] {
 
-    let map = {};
-    for(let index in list1) {
-        map[list1[index]] = Number(index);
-    }
-
-    let arr = [];
-    for(let index in list2){
-        if(typeof map[list2[index]] !== 'undefined'){
-            arr.push([list2[index], map[list2[index]]+Number(index)]);
+    let res = [];
+    let obj = new Map();
+    list2.forEach((v, i) => obj.set(v, i));
+    
+    let i = 0, min = Infinity;
+    while(i < list1.length){
+        if(obj.has(list1[i])){
+            let sum = i + obj.get(list1[i]);
+            if(min === sum){
+                res.push(list1[i]);
+            }   
+            else if(sum < min){
+                res = [list1[i]];
+                min = sum;
+            }
         }
+        i++;
     }
     
-    return arr.sort((a,b) => (a[1] <= b[1]) ? -1 : 1).filter(x => x[1] == arr[0][1]).map(x => x[0])
+    return res;
 };
