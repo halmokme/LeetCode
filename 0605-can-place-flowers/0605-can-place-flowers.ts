e4f09afddae754cb539118f29f72f2da4e71c96d
김재훈 <107684690+halmokme@users.noreply.github.com>
2023-05-02 20:32:02 +0900

    Time: 76 ms (28.59%), Space: 45.6 MB (15.12%) - LeetHub

---
+++ 0605-can-place-flowers/0605-can-place-flowers.ts
@@ -0,0 +1,14 @@
+function canPlaceFlowers(flowerbed: number[], n: number): boolean {
+
+    if(n === 0) return true;
+
+    let count = 0;
+    for(let i=0; i<flowerbed.length; i++) {
+        if(flowerbed[i-1] !== 1 && flowerbed[i] !== 1 && flowerbed[i+1] !== 1) {
+            flowerbed[i] = 1;
+            count++;
+            if(count === n) break;
+        }
+    }
+    return count === n;
+}
