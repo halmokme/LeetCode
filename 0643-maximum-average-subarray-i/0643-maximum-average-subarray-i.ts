b5c75b869fdab393b5b40e35dc048a1418c29cba
김재훈 <107684690+halmokme@users.noreply.github.com>
2023-05-04 00:45:08 +0900

    Time: 81 ms (98.57%), Space: 56.7 MB (32.14%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.ts
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.ts
@@ -7,10 +7,9 @@ function findMaxAverage(nums: number[], k: number): number {
 
   let maxSum = currSum, left = 0;
   for(let i=k; i<nums.length; i++) {
-      currSum -= nums[left];
+      currSum -= nums[left++];
       currSum += nums[i];
       if(currSum > maxSum) maxSum = currSum;
-      left++;
   }
 
   return maxSum / k;
