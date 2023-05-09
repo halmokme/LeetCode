0a164627bdea55b637fd2fcc7992e6b8d65e7abf
김재훈 <107684690+halmokme@users.noreply.github.com>
2023-05-10 00:04:08 +0900

    Time: 57 ms (80.95%), Space: 42.8 MB (61.90%) - LeetHub

--- 0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.ts
+++ 0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.ts
@@ -2,8 +2,7 @@ function hasAlternatingBits(n: number): boolean {
     let binary = n.toString(2);
 
     for(let i=1; i<binary.length; i++) {
-        if(binary[i] !== binary[i-1]) continue;
-        else return false;
+        if(binary[i] === binary[i-1]) return false;
     }
     
     return true;
