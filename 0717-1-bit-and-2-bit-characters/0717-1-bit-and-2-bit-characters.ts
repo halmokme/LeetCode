3f7056d72d4f5f37eee25c8c0570277fb6ae82ec
김재훈 <107684690+halmokme@users.noreply.github.com>
2023-05-21 23:59:14 +0900

    Time: 71 ms (17.65%), Space: 44.8 MB (47.06%) - LeetHub

--- 0717-1-bit-and-2-bit-characters/0717-1-bit-and-2-bit-characters.ts
+++ 0717-1-bit-and-2-bit-characters/0717-1-bit-and-2-bit-characters.ts
@@ -1,7 +1,7 @@
 function isOneBitCharacter(bits: number[]): boolean {
     for(let i=0;i<bits.length;){
        if(bits[i]===0){
-           if(i===bits.length-1&& bits[i]===0) return true;
+           if(i===bits.length-1 && bits[i]===0) return true;
            i++;
        }
         else i+=2;
