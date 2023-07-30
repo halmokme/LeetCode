407981e3dd9fc8e6d9ba2a815c2d16371d31c52e
김재훈 <107684690+halmokme@users.noreply.github.com>
2023-07-30 11:14:35 +0900

    Time: 55 ms (99.34%), Space: 44.5 MB (57.84%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const pivotIndex = function(nums) {
+    // ls(left sum), rs(right sum)
+    let ls= 0, rs= nums.reduce((a, b) => a + b) - nums[0];
+    
+    if(ls === rs) return 0;
+    
+    for(let i=1; i<nums.length; i++) {
+        ls += nums[i-1]; 
+        rs -= nums[i]; 
+        if(ls === rs) return i;
+    }
+    
+    return -1
+};
